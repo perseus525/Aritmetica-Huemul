4673264072bc6b053ddc515644b48bdd3b933fea
perseus525 <37369372+perseus525@users.noreply.github.com>
2018-05-22 11:48:08 -0400

    Se renmbran las clases y metodos

--- src/main/java/App/App.java
+++ src/main/java/App/App.java
@@ -12,4 +12,29 @@ package App;
  */
 public class App {
     
+  public void llenarSuma(int[][] matriz){
+  
+      int llenado;
+      
+      int base =matriz.length;
+      
+      for(int i =0; i<matriz.length;i++){
+          
+           for(int j =0; j<matriz.length;i++){
+          
+          llenado = i + j;
+          
+          llenado = revisar(llenado, base);
+          
+          matriz[i][j]=llenado;
+        }    
+      }
+  }  
+  public int revisar(int n, int base){
+      while( n >= base){
+          
+          n=n-base;
+      }
+      return n;
+  }
 }
